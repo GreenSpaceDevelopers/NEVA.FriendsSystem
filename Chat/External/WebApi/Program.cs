aa905d7edbd9c2f72b3ae79c95d58dfa4a9ba2cf
artsiom.herasimuik <artem.gerasimuik@icloud.com>
2025-06-27 04:06:42 +0300

    push

--- Chat/External/WebApi/Program.cs
+++ Chat/External/WebApi/Program.cs
@@ -33,15 +33,14 @@ public static class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline
-        if (app.Environment.IsDevelopment())
-        {
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chat API V1");
                 c.RoutePrefix = "swagger";
             });
-        }
+        
 
         app.UsePathBase("/api");
 
