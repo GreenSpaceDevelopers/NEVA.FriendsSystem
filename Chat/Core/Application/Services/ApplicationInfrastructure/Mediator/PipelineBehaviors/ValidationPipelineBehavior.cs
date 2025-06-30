82eb6a8eaa2b76c3b29c517334e00a1bfca43569
artsiom.herasimuik <artem.gerasimuik@icloud.com>
2025-06-30 12:30:51 +0300

    fix

--- Chat/Core/Application/Services/ApplicationInfrastructure/Mediator/PipelineBehaviors/ValidationPipelineBehavior.cs
+++ Chat/Core/Application/Services/ApplicationInfrastructure/Mediator/PipelineBehaviors/ValidationPipelineBehavior.cs
@@ -4,17 +4,24 @@ using FluentValidation;
 
 namespace Application.Services.ApplicationInfrastructure.Mediator.PipelineBehaviors;
 
-public class ValidationPipelineBehavior<TRequest>(IValidator<TRequest> validator) : IPipelineBehavior<TRequest> where TRequest : IRequest
+public class ValidationPipelineBehavior<TRequest>(IEnumerable<IValidator<TRequest>> _validators) : IPipelineBehavior<TRequest> where TRequest : IRequest
 {
     public async Task<IOperationResult> HandleAsync(TRequest request, Func<Task<IOperationResult>> next, CancellationToken cancellationToken)
     {
-        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        var context = new ValidationContext<TRequest>(request);
+        var failures = _validators
+            .Select(validator => validator.Validate(context))
+            .SelectMany(result => result.Errors)
+            .Where(failure => failure != null)
+            .ToArray();
 
-        if (!validationResult.IsValid)
+        if (failures.Length == 0)
         {
-            return ResultsHelper.BadRequest(validationResult.Errors.Select(e => e.ErrorMessage).Aggregate((e1, e2) => $"{e1}, {e2}"));
+            return await next();
         }
         
-        return await next();
+        var result = ResultsHelper.BadRequest(failures.Select(failure => failure.ErrorMessage).Aggregate((er1, er2) => er1 + ", " + er2));
+        
+        return result;
     }
 }
